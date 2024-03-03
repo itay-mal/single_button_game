77958737808b2fc371c2935fa26dfbb1741e259f
itay-mal <itay.mal@campus.technion.ac.il>
2024-03-03 22:33:07 +0200

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,5 @@
 import pygame
-
-    
+import time
 
 class ScoreBoard:
     def __init__(self, screen: pygame.Surface,
@@ -12,8 +11,11 @@ class ScoreBoard:
         self.score = initial_score
         self.box_width = 30
         self.box_height = 10
+        self.last_score_add = time.time()
+        self.screen_width = screen_width
 
     def show(self) -> None:
+        # lives
         rect = pygame.Rect(0, 0, self.box_width, self.box_height)
         pygame.draw.rect(self.screen, (0,0,0), rect)
         for i in range(self.lives):
@@ -22,9 +24,18 @@ class ScoreBoard:
                             self.box_height-2,
                             (self.box_width/3) - 2)
 
+        # score
+        textRect = pygame.Rect(self.screen_width - self.box_width, 0, self.box_width, self.box_height)
+        font = pygame.font.SysFont('Comic Sans MS', 10)
+        text = font.render(f"{self.score:05}", False, (255,255,255), (0,0,0))
+        self.screen.blit(text, textRect)
+
     def update(self, wall_coll:bool, bug_coll:bool) -> bool:
         self.lives -= bug_coll and not self.bug_coll_p
         self.bug_coll_p = bug_coll
+        if time.time() - self.last_score_add > 1:
+            self.score += 1
+            self.last_score_add = time.time()
         return not self.lives or wall_coll
 
 
